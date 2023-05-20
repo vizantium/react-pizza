65ebc1e5c64c6eec0f3ca2e9478c880db6f206ef
Alexander Balan <103833773+vizantium@users.noreply.github.com>
2023-05-20 10:37:08 +0300

    Update Header.tsx

--- src/Components/Header/Header.tsx
+++ src/Components/Header/Header.tsx
@@ -34,7 +34,7 @@ export const Header:React.FC = () => {
                 <div className="header__cart">
                 {pathname !== '/cart' && (
                     <Link to="/cart" className="button button--cart">
-                        <span>{totalPrice} ₽</span>
+                        <span>{totalPrice} ₸</span>
                         <div className="button__delimiter"></div>
                         <svg
                             width="18"
