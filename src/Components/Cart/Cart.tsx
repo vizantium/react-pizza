f6c05cee783ffe277600bf60d88c3b45d87194f4
Alexander Balan <103833773+vizantium@users.noreply.github.com>
2023-05-20 10:37:28 +0300

    Update Cart.tsx

--- src/Components/Cart/Cart.tsx
+++ src/Components/Cart/Cart.tsx
@@ -65,7 +65,7 @@ const Cart: React.FC = () => {
             <div className="cart__bottom">
                 <div className="cart__bottom-details">
                     <span> Всего пицц: <b>{totalCount} шт.</b> </span>
-                    <span> Сумма заказа: <b>{totalPrice} ₽</b> </span>
+                    <span> Сумма заказа: <b>{totalPrice} ₸</b> </span>
                 </div>
                 <div className="cart__bottom-buttons">
                     <Link to="/" className="button button--outline button--add go-back-btn">
